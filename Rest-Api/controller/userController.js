a53a7f0b5cedbf8b523f7b1ae2182ac53f9ce12b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-19 00:53:06 +0300

    Create userController.js

---
+++ Rest-Api/controller/userController.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+
+const userServices = require('../services/userServices');
+
+const { COOKIE_SESION_NAME } = require('../constants');
+
+router.post('/register', async (req, res) => {
+
+    try {
+
+        const { username, email, password } = req.body;
+        const user = await userServices.register(username, email, password);
+        const token = await userServices.createToken(user);
+
+        // res.cookie(COOKIE_SESION_NAME, token, { httpOnly: true });
+        res.json({
+            authToken: token,
+            username: user.username,
+            email: user.email,
+            password: user.password,
+            _id: user._id,
+        })
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({
+            message: error.message
+        });
+    }
+});
+
+router.post('/login', async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+        const user = await userServices.login(email, password);
+        const token = await userServices.createToken(user);
+
+        // res.cookie(COOKIE_SESION_NAME, token, { httpOnly: true });
+        res.json({
+            authToken: token,
+            email: user.email,
+            _id: user._id,
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+
+});
+
+router.get('/logout', (req, res) => {
+
+    // res.clearCookie(COOKIE_SESION_NAME);
+    res.json({});
+    res.status(200);
+});
+
+
+module.exports = router;
