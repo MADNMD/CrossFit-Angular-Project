197495c37e53e27bf078379c69ef38e1b67ea63d
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-20 18:26:18 +0300

    add header component

--- crossFit-app/src/app/core/core.module.ts
+++ crossFit-app/src/app/core/core.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    HeaderComponent,
     FooterComponent,
   ]
 })
