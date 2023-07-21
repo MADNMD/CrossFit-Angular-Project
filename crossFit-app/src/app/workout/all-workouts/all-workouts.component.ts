14b47bcabdeaece34b2cd06f746c338fe05dbcac
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-21 17:59:58 +0300

    add all-workouts component

---
+++ crossFit-app/src/app/workout/all-workouts/all-workouts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-workouts',
+  templateUrl: './all-workouts.component.html',
+  styleUrls: ['./all-workouts.component.css']
+})
+export class AllWorkoutsComponent {
+
+}
--- crossFit-app/src/app/workout/workout.module.ts
+++ crossFit-app/src/app/workout/workout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AllWorkoutsComponent } from './all-workouts/all-workouts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AllWorkoutsComponent
+  ],
   imports: [
     CommonModule
   ]
