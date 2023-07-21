bcd599b9a5e749e7ccf934b730ee4bec2b6c9e46
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-21 17:50:19 +0300

    add user routing module

---
+++ crossFit-app/src/app/user/user-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+// import { PageNotFoundComponent } from '../core/page-not-found/page-not-found.component';
+// import { CreateWorkoutComponent } from '../workout/create-workout/create-workout.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  // 
+  // {
+  //   path: 'workouts/create',
+  //   component: CreateWorkoutComponent
+  // },
+  // {
+  //   path: 'not-found',
+  //   component: PageNotFoundComponent,
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '/not-found'
+  // }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
