61e742a3586482b2adb45a1e08965a7e8291bbb5
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-22 18:57:36 +0300

    Update auth.activate.ts

---
+++ crossFit-app/src/app/core/guards/auth.activate.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { CanActivate, Router, UrlTree } from "@angular/router";
+import { UserService } from "src/app/user/user.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthActivate implements CanActivate {
+
+    constructor(private userService: UserService, private router: Router) { }
+    // Този клас AuthActivate отиваме и го слагаме в рутера на User така canActivate: [AuthActivate] и в theme модула на addTheme също го добавяме;
+    canActivate(): boolean | UrlTree {
+
+        if (this.userService.user) {
+            return true;
+        } else {
+            const loginUrlTree: UrlTree = this.router.parseUrl('/login');
+            return loginUrlTree
+        }
+    }
+
+}
