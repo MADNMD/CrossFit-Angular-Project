6f1bb89a33a26f079e84a1a6f783409d6d41ce84
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-23 17:29:44 +0300

    Update register.component.ts

--- crossFit-app/src/app/user/register/register.component.ts
+++ crossFit-app/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(
+    private userService: UserService, 
+    private formBuilder: FormBuilder, 
+    private router: Router) { }
+
+  registerForm: FormGroup = this.formBuilder.group({
+    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    repeatPassword: new FormControl('', [Validators.required]),
+  })
+
+  registerUser(): void {
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const { username, email, password } = this.registerForm.value;
+    this.userService.registerUser(username, email, password).subscribe(() => {
+      this.router.navigateByUrl('/workouts/allWorouts');
+      this.registerForm.reset();
+    },
+      (error) => {
+        console.error(error);
+      }
+    )
+  }
+
+  comparePasswords(): boolean {
+    return this.registerForm.value.password === this.registerForm.value.repeatPassword;
+  }
 }
