5112162d1a89cff5018449dbe8f86b9dfbddf578
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-23 17:16:51 +0300

    change logic

--- Rest-Api/middlewares/authMiddleware.js
+++ Rest-Api/middlewares/authMiddleware.js
@@ -5,41 +5,41 @@ const { COOKIE_SESION_NAME } = require('../constants');
 
 exports.auth = (req, res, next) => {
 
-    // const token = req.cookies[COOKIE_SESION_NAME];
-
-    // if (token) {
-    //     jwt.verify(token, SECRET, ((err, decodedToken) => {
-    //         if (err) {
-    //             res.clearCookie(COOKIE_SESION_NAME);
-
-    //             res.redirect('/auth/login');
-    //         }
-
-    //         req.user = decodedToken;
-    //         res.locals.user = decodedToken;
-
-    //         next();
-    //     }));
-    // } else {
-    //     next();
-    // }
-
-    const token = req.header('Authorization');
+    const token = req.cookies[COOKIE_SESION_NAME];
 
     if (token) {
+        jwt.verify(token, SECRET, ((err, decodedToken) => {
+            if (err) {
+                res.clearCookie(COOKIE_SESION_NAME);
 
-        try {
-            const decodedToken = jwt.verify(token, SECRET);
+                res.redirect('/auth/login');
+            }
 
             req.user = decodedToken;
+            res.locals.user = decodedToken;
 
             next();
-        } catch (error) {
-            res.status(401).json({
-                message: 'You are not auhtorized!',
-            })
-        }
+        }));
     } else {
         next();
     }
+
+    // const token = req.header('Authorization');
+
+    // if (token) {
+
+    //     try {
+    //         const decodedToken = jwt.verify(token, SECRET);
+
+    //         req.user = decodedToken;
+
+    //         next();
+    //     } catch (error) {
+    //         res.status(401).json({
+    //             message: 'You are not auhtorized!',
+    //         })
+    //     }
+    // } else {
+    //     next();
+    // }
 }
