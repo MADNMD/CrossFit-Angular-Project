b51e5b9cb0d350976ccf590700390d6affd006c1
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-23 17:25:56 +0300

    Update login.component.ts

--- crossFit-app/src/app/user/login/login.component.ts
+++ crossFit-app/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router,) { }
+
+  loginForm: FormGroup = this.formBuilder.group({
+    email: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z]+@[A-Za-z]+\.[A-Za-z]+$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  loginUser(): void {
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+    this.userService.loginUser(email, password).subscribe(() => {
+      this.router.navigateByUrl('/workouts/allWorouts');
+      this.loginForm.reset();
+    },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
 }
