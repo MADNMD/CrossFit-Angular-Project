09f4656159dd36ca9d48f538b32660772340bb3a
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-24 18:58:40 +0300

    Update all-workouts.component.ts

--- crossFit-app/src/app/workout/all-workouts/all-workouts.component.ts
+++ crossFit-app/src/app/workout/all-workouts/all-workouts.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkoutService } from '../workout.service';
+import { Workout } from 'src/app/interfaces/workout';
 
 @Component({
   selector: 'app-all-workouts',
   templateUrl: './all-workouts.component.html',
   styleUrls: ['./all-workouts.component.css']
 })
-export class AllWorkoutsComponent {
+export class AllWorkoutsComponent implements OnInit{
+
+  allWorkouts: Workout[] = [];
+
+  constructor(private workoutService: WorkoutService) {}
+
+  ngOnInit(): void {
+    this.workoutService.getAllWorkouts().subscribe({
+      next: (workouts) => {
+        // console.log(workouts)
+        this.allWorkouts = workouts;
+      },
+      // error: (error) => {
+      //   console.log(`Error ${error}`);
+      // }
+    });
+  }
 
 }
