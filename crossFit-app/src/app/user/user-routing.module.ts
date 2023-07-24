40299f4443107ad6071a2883b4aec35516795232
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-24 18:52:51 +0300

    delete imports

--- crossFit-app/src/app/user/user-routing.module.ts
+++ crossFit-app/src/app/user/user-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-// import { PageNotFoundComponent } from '../core/page-not-found/page-not-found.component';
-// import { CreateWorkoutComponent } from '../workout/create-workout/create-workout.component';
 
 const routes: Routes = [
   {
