735c0c02e9c702f64d2121c5f41644baae80ce81
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-24 18:41:05 +0300

    Update authenticate.component.ts

--- crossFit-app/src/app/authenticate/authenticate.component.ts
+++ crossFit-app/src/app/authenticate/authenticate.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
   styleUrls: ['./authenticate.component.css']
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+
+  isAuthenticated = true;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+
+    this.userService.getUser().subscribe({
+      next: () => { this.isAuthenticated = false },
+      error: () => { this.isAuthenticated = false },
+    })
+  }
 
 }
