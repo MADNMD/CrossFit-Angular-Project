8c0c31c7a72ee7defd8661d00f078e789f22f852
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-25 18:27:33 +0300

    Update user-routing.module.ts

--- crossFit-app/src/app/user/user-routing.module.ts
+++ crossFit-app/src/app/user/user-routing.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthPrevent } from '../core/guards/auth.prevent';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AuthPrevent]
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [AuthPrevent]
   }
 ];
 
