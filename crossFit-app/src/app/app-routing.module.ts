2cae2f493705a47fa94406bf16b448d0c19ccc29
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-25 18:19:05 +0300

    Update app-routing.module.ts

--- crossFit-app/src/app/app-routing.module.ts
+++ crossFit-app/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./user/user.module').then(module => module.UserModule)
+  },
+  {
+    path: 'workouts',
+    loadChildren: () => import('./workout/workout.module').then(module => module.WorkoutModule)
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
