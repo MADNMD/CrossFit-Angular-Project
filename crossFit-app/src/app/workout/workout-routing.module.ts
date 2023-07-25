612c4f5a1eaec4462b13ebd6bdaf149e15274f2b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-25 18:19:51 +0300

    Update workout-routing.module.ts

--- crossFit-app/src/app/workout/workout-routing.module.ts
+++ crossFit-app/src/app/workout/workout-routing.module.ts
@@ -9,32 +9,27 @@ import { MyWorkoutsComponent } from './my-workouts/my-workouts.component';
 
 const routes: Routes = [
     {
-        path: 'workouts',
-        children: [
-            {
-                path: 'create',
-                component: CreateWorkoutComponent,
-                canActivate: [AuthActivate],
-            },
-            {
-                path: 'allWorouts',
-                component: AllWorkoutsComponent,
-            },
-            {
-                path: 'details/:workoutId',
-                component: DetailsWorkoutComponent,
-            },
-            {
-                path: 'edit/:workoutId',
-                component: EditWorkoutComponent,
-                canActivate: [AuthActivate,]
-            },
-            {
-                path: 'my-workouts',
-                component: MyWorkoutsComponent,
-                canActivate: [AuthActivate],
-            }
-        ]
+        path: 'create',
+        component: CreateWorkoutComponent,
+        canActivate: [AuthActivate],
+    },
+    {
+        path: 'allWorouts',
+        component: AllWorkoutsComponent,
+    },
+    {
+        path: 'details/:workoutId',
+        component: DetailsWorkoutComponent,
+    },
+    {
+        path: 'edit/:workoutId',
+        component: EditWorkoutComponent,
+        canActivate: [AuthActivate,]
+    },
+    {
+        path: 'my-workouts',
+        component: MyWorkoutsComponent,
+        canActivate: [AuthActivate],
     }
 ];
 
@@ -43,3 +38,51 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 export class WorkoutRoutingModule { }
+
+
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { CreateWorkoutComponent } from './create-workout/create-workout.component';
+// import { AllWorkoutsComponent } from './all-workouts/all-workouts.component';
+// import { DetailsWorkoutComponent } from './details-workout/details-workout.component';
+// import { EditWorkoutComponent } from './edit-workout/edit-workout.component';
+// import { AuthActivate } from '../core/guards/auth.activate';
+// import { MyWorkoutsComponent } from './my-workouts/my-workouts.component';
+
+// const routes: Routes = [
+//     {
+//         path: 'workouts',
+//         children: [
+//             {
+//                 path: 'create',
+//                 component: CreateWorkoutComponent,
+//                 canActivate: [AuthActivate],
+//             },
+//             {
+//                 path: 'allWorouts',
+//                 component: AllWorkoutsComponent,
+//             },
+//             {
+//                 path: 'details/:workoutId',
+//                 component: DetailsWorkoutComponent,
+//             },
+//             {
+//                 path: 'edit/:workoutId',
+//                 component: EditWorkoutComponent,
+//                 canActivate: [AuthActivate,]
+//             },
+//             {
+//                 path: 'my-workouts',
+//                 component: MyWorkoutsComponent,
+//                 canActivate: [AuthActivate],
+//             }
+//         ]
+//     }
+// ];
+
+// @NgModule({
+//     imports: [RouterModule.forChild(routes)],
+//     exports: [RouterModule]
+// })
+// export class WorkoutRoutingModule { }
