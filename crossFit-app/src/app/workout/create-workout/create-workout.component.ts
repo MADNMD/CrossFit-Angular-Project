575178de6be3dad097b25ff81e24491c1b09dfda
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-25 18:12:36 +0300

    Update create-workout.component.ts

--- crossFit-app/src/app/workout/create-workout/create-workout.component.ts
+++ crossFit-app/src/app/workout/create-workout/create-workout.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { WorkoutService } from '../workout.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-create-workout',
@@ -7,4 +12,40 @@ import { Component } from '@angular/core';
 })
 export class CreateWorkoutComponent {
 
+  get user(): User | null {
+    return this.userService.user;
+
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private workoutService: WorkoutService,
+    private router: Router,
+    private userService: UserService) { }
+
+  createForm: FormGroup = this.formBuilder.group({
+    difficultyLevel: new FormControl('', [Validators.required,]),
+    typeTraining: new FormControl('', [Validators.required]),
+    image: new FormControl('', [Validators.required, Validators.pattern('^(http|https)://.+')]),
+    trainingDuration: new FormControl('', [Validators.required]),
+    exercises: new FormControl('', [Validators.required]),
+  });
+
+  createWorkout(): void {
+
+    if (this.createForm.invalid) {
+      return;
+    }
+
+    const { difficultyLevel, typeTraining, image, trainingDuration, exercises } = this.createForm.value;
+    const owner: User | null = this.user;
+    // console.log(owner)
+    if (owner) {
+      const likes: string[] = [];
+      this.workoutService.createWorkout(difficultyLevel, typeTraining, image, trainingDuration, exercises, likes, owner).subscribe(() => {
+        this.router.navigateByUrl('/workouts/allWorouts');
+        this.createForm.reset();
+      });
+    }
+  }
 }
