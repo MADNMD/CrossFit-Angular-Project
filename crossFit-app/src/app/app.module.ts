f8b4d52dd08fea4cb46726437814e912bf861d02
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-25 18:22:22 +0300

    Update app.module.ts

--- crossFit-app/src/app/app.module.ts
+++ crossFit-app/src/app/app.module.ts
@@ -4,21 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { UserModule } from './user/user.module';
+import { RouterModule } from '@angular/router';
+// import { UserModule } from './user/user.module';
+// import { WorkoutModule } from './workout/workout.module';
+import { HttpClientModule } from '@angular/common/http';
+import { appInterceptorProvider } from './app.interceptor';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticateComponent
+    AuthenticateComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
-    UserModule,
-    AppRoutingModule
+    // UserModule,
+    // WorkoutModule,
+    RouterModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [appInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
