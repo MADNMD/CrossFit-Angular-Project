2f4381efb4930b6b7e36e0a41d7e6452550aa917
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-30 18:51:49 +0300

    add delete user routs

--- Rest-Api/controller/userController.js
+++ Rest-Api/controller/userController.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const userServices = require('../services/userServices');
 
 const { COOKIE_SESION_NAME } = require('../constants');
+const { getErrorMessage } = require('../utils/errorHellper');
 
 router.post('/register', async (req, res) => {
 
@@ -80,17 +81,36 @@ router.put('/edit/:userId', async (req, res) => {
     try {
 
         const userId = req.user._id;
-        const {username, email} = req.body;
+        const { username, email } = req.body;
 
-        const updateUserData = {username, email};
-       
+        const updateUserData = { username, email };
+
         const editUser = await userServices.editUser(userId, updateUserData);
 
         res.status(200).json(editUser)
     } catch (error) {
         res.status(400).json({
-            message: error.message
+            message: getErrorMessage(error)
         });
+    }
+});
+
+router.delete('/delete/:userId', async (req, res) => {
+
+    try {
+
+        // const userId = req.params.userId;
+        const userId = req.user._id;
+        console.log('Received delete request for userId:', userId);
+        console.log(userId);
+        await userServices.deleteUser(userId);
+        console.log('User deleted successfully.');
+        res.clearCookie(COOKIE_SESION_NAME);
+        res.status(200).end();
+    } catch (error) {
+        res.status(400).json({
+            message: getErrorMessage(error)
+        })
     }
 });
 
