aa5081762846e22dc464897c312f997444ba31bf
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-30 19:05:59 +0300

    Update user-routing.module.ts

--- crossFit-app/src/app/user/user-routing.module.ts
+++ crossFit-app/src/app/user/user-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthPrevent } from '../core/guards/auth.prevent';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthActivate } from '../core/guards/auth.activate';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
   {
@@ -14,6 +17,16 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
     canActivate: [AuthPrevent]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthActivate]
+  },
+  {
+    path: 'edit-profile',
+    component: EditProfileComponent,
+    canActivate: [AuthActivate],
   }
 ];
 
