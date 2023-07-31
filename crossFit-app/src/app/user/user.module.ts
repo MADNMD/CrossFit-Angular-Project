c9cdf487ba4b82f2a2f4a9079a037d6a31e166c8
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-31 19:24:29 +0300

    add delete dialog component and add  matDialogModul and matButtonModule

--- crossFit-app/src/app/user/user.module.ts
+++ crossFit-app/src/app/user/user.module.ts
@@ -7,6 +7,9 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { DeleteConfirmDialogComponent } from './delete-confirm-dialog/delete-confirm-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -15,18 +18,22 @@ import { EditProfileComponent } from './edit-profile/edit-profile.component';
     RegisterComponent,
     ProfileComponent,
     EditProfileComponent,
+    DeleteConfirmDialogComponent,
   ],
   imports: [
     CommonModule,
     UserRoutingModule,
     RouterModule,
     ReactiveFormsModule,
+    MatDialogModule,
+    MatButtonModule,
   ],
   exports: [
     LoginComponent,
     RegisterComponent,
     ProfileComponent,
     EditProfileComponent,
+    DeleteConfirmDialogComponent,
   ]
 })
 export class UserModule { }
