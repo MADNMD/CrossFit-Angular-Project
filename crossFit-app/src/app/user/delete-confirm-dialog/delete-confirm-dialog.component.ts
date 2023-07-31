067fb63bc047489f68ebc21f9599edd9247be219
Mihail Donchev <mihaildonchev88@abv.bg>
2023-07-31 19:27:23 +0300

    Update delete-confirm-dialog.component.ts

--- crossFit-app/src/app/user/delete-confirm-dialog/delete-confirm-dialog.component.ts
+++ crossFit-app/src/app/user/delete-confirm-dialog/delete-confirm-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-confirm-dialog',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-confirm-dialog.component.css']
 })
 export class DeleteConfirmDialogComponent {
+
+  constructor(private dialogRef: MatDialogRef<DeleteConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string, message: string }) { }
 
 }
