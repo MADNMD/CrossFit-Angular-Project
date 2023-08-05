22573d3349a5ad406ce0ece943fbb895775fcc57
Mihail Donchev <mihaildonchev88@abv.bg>
2023-08-05 15:55:02 +0300

    change database name

--- Rest-Api/config/env.js
+++ Rest-Api/config/env.js
@@ -1,5 +1,5 @@
 exports.PORT = 3000;
-exports.DB_CONECTION_URL = 'mongodb+srv://mihaildonchev88:mQvMqaSVOqXR8oDb@dev-app-cluster.i86lglg.mongodb.net/?retryWrites=true&w=majority';
+exports.DB_CONECTION_URL = 'mongodb+srv://mihaildonchev88:mQvMqaSVOqXR8oDb@dev-app-cluster.i86lglg.mongodb.net/crossFit';
 // exports.DB_CONECTION_URL = 'mongodb://localhost:27017/crossFit';
 exports.SECRET = 'dash3-16-64fassd-3-97-64dsdada';
 exports.SALT_ROUNDS = 10;
