9d28a948bc111973977f39d19d3c4958c42e909b
Mihail Donchev <mihaildonchev88@abv.bg>
2023-08-05 16:41:44 +0300

    Update environment.ts

--- crossFit-app/src/environments/environment.ts
+++ crossFit-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    appUrl: 'https://crossfir-app-rest-api.onrender.com/api'
+    appUrl: 'https://crossfir-app-rest-api.onrender.com'
     // appUrl: 'http://localhost:3000'
 };
