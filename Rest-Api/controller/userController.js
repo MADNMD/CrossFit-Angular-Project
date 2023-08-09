acfa6ea53ac2056e44ebd031e0477f4f525aa722
Mihail Donchev <mihaildonchev88@abv.bg>
2023-08-10 01:15:54 +0300

    Update userController.js

--- Rest-Api/controller/userController.js
+++ Rest-Api/controller/userController.js
@@ -117,7 +117,7 @@ router.delete('/delete/:userId', async (req, res) => {
 
 router.get('/logout', (req, res) => {
 
-    res.clearCookie(COOKIE_SESION_NAME, { maxAge: 0 });
+    res.clearCookie(COOKIE_SESION_NAME);
     res.json({});
     res.status(200);
 });
