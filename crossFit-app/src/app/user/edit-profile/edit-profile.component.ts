9dbd5d63ac05faf852269996e10cae7947deba0f
Mihail Donchev <mihaildonchev88@abv.bg>
2023-08-12 18:38:11 +0300

    add error message

--- crossFit-app/src/app/user/edit-profile/edit-profile.component.ts
+++ crossFit-app/src/app/user/edit-profile/edit-profile.component.ts
@@ -5,6 +5,7 @@ import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { appEmailvalidator } from 'src/app/shared/validators/app-email.validator';
 import { ErrorMessageService, MessageType } from 'src/app/core/error-message.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-profile',
@@ -15,9 +16,15 @@ export class EditProfileComponent implements OnInit {
 
   user: User | undefined;
 
+  message: string | null = null;
+  isMessageError: boolean | null = null;
+
+  private subscription!: Subscription;
+
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
+    private errorMessageService: ErrorMessageService,
     private router: Router) { }
 
   editUser: FormGroup = this.formBuilder.group({
@@ -27,6 +34,18 @@ export class EditProfileComponent implements OnInit {
   })
 
   ngOnInit(): void {
+
+    this.subscription = this.errorMessageService.onNewMessage$.subscribe((message) => {
+      this.message = message.text;
+      this.isMessageError = message.type === MessageType.Error;
+
+      if (this.message && this.isMessageError) {
+        setTimeout(() => {
+          this.message = null;
+          this.isMessageError = null;
+        }, 3000);
+      }
+    });
 
     if (this.userService.user) {
       this.user = this.userService.user;
@@ -51,9 +70,9 @@ export class EditProfileComponent implements OnInit {
       this.userService.editUser(this.user?._id, username, email).subscribe(() => {
         this.router.navigateByUrl('/auth/profile')
       },
-      (error) => {
-        console.error('Error editing user:', error);
-      })
+        (error) => {
+          console.error('Error editing user:', error);
+        })
     }
 
   }
